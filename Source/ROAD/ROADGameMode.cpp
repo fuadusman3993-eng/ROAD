c9628440bfdd29c075e5952c5fa9375aa408bc9f
fuadusman3993-eng <fuadusman3993@gmail.com>
2026-05-07 00:09:09 +0300

    Fix build and deploy

--- Source/ROAD/ROADGameMode.cpp
+++ Source/ROAD/ROADGameMode.cpp
@@ -1,7 +1,5 @@
 #include "ROADGameMode.h"
 #include "ROADPlayerController.h"
 
-AROADGameMode::AROADGameMode()
-{
-	PlayerControllerClass = AROADPlayerController::StaticClass();
-}
+AROADGameMode::AROADGameMode() {
+  PlayerControllerClass = AROADPlayerController::StaticClass();
